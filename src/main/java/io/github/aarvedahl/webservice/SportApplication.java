3236194bdb77b9ed798bdfea57a494e1ddd725a6
Alex <alex.arvedahl@gmail.com>
2017-11-08 16:13:11 +0100

    Returning a list with one sport and one match

---
+++ src/main/java/io/github/aarvedahl/webservice/SportApplication.java
@@ -0,0 +1,8 @@
+package io.github.aarvedahl.webservice;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("")
+public class SportApplication extends Application {
+}
---
+++ src/main/java/io/github/aarvedahl/webservice/SportService.java
@@ -0,0 +1,35 @@
+package io.github.aarvedahl.webservice;
+
+import com.sportsing.api.Match;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
+
+@Path("/sportservice")
+public class SportService {
+
+    private List<Match>matches = initMatches();
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getString() {
+        return "test123";
+    }
+
+    @GET
+    @Path("/list")
+    @Produces(MediaType.APPLICATION_XML)
+    public List<Match> getMatches() {
+
+        return matches;
+    }
+
+    private List<Match> initMatches() {
+        List<Match> sports = new ArrayList<>();
+        Match match = new Match("Ice Hockey");
+        match.registerContender("Alex", 3, 4);
+        sports.add(match);
+        return sports;
+    }
+}
